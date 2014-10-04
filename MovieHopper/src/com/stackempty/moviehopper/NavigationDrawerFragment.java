4017801c0fea47a31e25535a7b25a508e7a4d384
Sarang Joshi <sarangj@msn.com>
2014-10-04 06:32:37 -0700

    Started work

--- MovieHopper/src/com/stackempty/moviehopper/NavigationDrawerFragment.java
+++ MovieHopper/src/com/stackempty/moviehopper/NavigationDrawerFragment.java
@@ -107,10 +107,8 @@ public class NavigationDrawerFragment extends Fragment {
 				});
 		mDrawerListView.setAdapter(new ArrayAdapter<String>(getActionBar()
 				.getThemedContext(), android.R.layout.simple_list_item_1,
-				android.R.id.text1, new String[] {
-						getString(R.string.title_section1),
-						getString(R.string.title_section2),
-						getString(R.string.title_section3), }));
+				android.R.id.text1, new String[] { "Atticus Liu",
+						"Rushil Patel", "Luke Lafountaine", "Sarang Joshi", "Wilson Zhao" }));
 		mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
 		return mDrawerListView;
 	}
